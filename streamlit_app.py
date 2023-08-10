204d8ad25c0b753f5b54a987e43ea49c380c2d14
cmurgalesnowflake <141932520+cmurgalesnowflake@users.noreply.github.com>
2023-08-10 15:49:49 +0200

    Update streamlit_app.py
    new header

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,5 @@ streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinash & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toas')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
