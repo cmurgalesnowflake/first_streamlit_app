efde4865b2b4e3527a58bb1422c80d85bde9c9c2
cmurgalesnowflake <141932520+cmurgalesnowflake@users.noreply.github.com>
2023-08-11 16:05:23 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,4 +23,4 @@ streamlit.dataframe(fruits_to_show)
 
 import request
 fruitvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruitvice_response)*
+streamlit.text(fruitvice_response)
