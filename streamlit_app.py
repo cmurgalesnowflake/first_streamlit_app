b960d5831b94d2a829ecc71ec91e5e3549323b39
cmurgalesnowflake <141932520+cmurgalesnowflake@users.noreply.github.com>
2023-08-14 14:22:53 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,3 +35,5 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
 import snowflake.connector
+
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
